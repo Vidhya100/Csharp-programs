90f9035b9a2a79cecfaadacd7e52030bcf8af9d7
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-27 16:36:31 +0530

    leapyear program

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LeapYear
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Year ");
+            string year = Console.ReadLine();
+
+            if (year.Length == 4)
+            {
+                Console.WriteLine(year.Length);
+                int yearint = Convert.ToInt32(year);
+                if (yearint % 4 == 0 && yearint % 100 != 0 || yearint % 400 == 0)
+                {
+
+                    Console.WriteLine(yearint + " is a Leap Year.");
+
+                }
+                else
+                {
+                    Console.WriteLine(yearint + " is not a Leap Year.");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Please enter valid year.");
+            }
+        }
+    }
+}
