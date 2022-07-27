d6da40e779fad4ea65c5262eecdbd5ebcfe28403
Vidhya100 <vidhyadarade1@gmail.com>
2022-07-27 17:56:59 +0530

    PowerOfTwo program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PowerOfTwo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the power value: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0 && num <= 31)
+            {
+                for (int i = 0; i <= num; i++)
+                {
+                    double result = Math.Pow(2, i);
+                    Console.WriteLine("2 to the power of"+i+ "is "+result);
+                }
+            }
+        }
+    }
+}
